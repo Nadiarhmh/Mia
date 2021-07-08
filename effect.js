87936f585b535016b5ee766b515ed2940abbb124
pangestujava <hidayatr1234@gmail.com>
2021-07-08 19:46:33 +0700

    image

--- effect.js
+++ effect.js
@@ -104,7 +104,7 @@ $('document').ready(function () {
 
 	$('#balloons_flying').click(function () {
 		$('.balloon-border').animate({ top: -250 }, 8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+		$('#b1,#b5,#b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
 		// $('#b3').addClass('balloons-rotate-behaviour-two');
 		// $('#b4').addClass('balloons-rotate-behaviour-one');
@@ -152,9 +152,9 @@ $('document').ready(function () {
 		$('#b7').attr('id', 'b77')
 		$('#b11').animate({ top: 240, left: vw - 350 }, 500);
 		$('#b22').animate({ top: 240, left: vw - 250 }, 500);
-		$('#b33').animate({ top: 80, left: vw - 150 }, 500);
-		$('#b44').animate({ top: 80, left: vw - 50 }, 500);
-		$('#b55').animate({ top: 80, left: vw + 50 }, 500);
+		$('#b33').animate({ top: 100, left: vw - 150 }, 500);
+		$('#b44').animate({ top: 100, left: vw - 50 }, 500);
+		$('#b55').animate({ top: 100, left: vw + 50 }, 500);
 		$('#b66').animate({ top: 240, left: vw + 150 }, 500);
 		$('#b77').animate({ top: 240, left: vw + 250 }, 500);
 		$('.balloons').css('opacity', '5.9');
